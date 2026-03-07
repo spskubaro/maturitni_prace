f73cb3823ef3ee9390e62d2fc6268c4f4fea1ed5
spskubaro <rodakjakub@sps-karvina.cz>
2026-03-07 22:31:52 +0100

    Final production fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
-﻿import { createRoot } from "react-dom/client";
-import App from "./App.tsx"
-import "./index.css"
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />)
+document.documentElement.lang = "cs";
+document.documentElement.setAttribute("translate", "no");
+document.documentElement.classList.add("notranslate");
+document.body.setAttribute("translate", "no");
+document.body.classList.add("notranslate");
 
+createRoot(document.getElementById("root")!).render(<App />);
